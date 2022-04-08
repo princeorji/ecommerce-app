927866e382f42ee71d6ef398692d1addd9d53f4d
Michael Orji <princemikeorji@gmail.com>
2022-04-08 13:58:10 +0100

    add to cart functionality

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.store, name="index"),
     path('product/<int:pk>/', views.product, name="product"),
     path('cart/', views.cart, name="cart"),
+    path('update_item/', views.update_item, name="update-item"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 # Create your views here.
@@ -29,3 +31,28 @@ def checkout(request):
     context = {}
     return render(request, '', context)
 """
+
+def update_item(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('productId:', productId)
+    print('action:', action)
+    
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete() 
+    return JsonResponse('item was added', safe=False) 
